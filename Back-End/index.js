b0c30aeccbd450d561b59d56f3acd85b357e56f1
Muhammad Umar Hassan <mumarh135@gmail.com>
2025-09-22 20:28:32 +0500

    Update index.js

--- Back-End/index.js
+++ Back-End/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/api/website/todo", router);
 
 // Port (fallback to 5000 if env is missing)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Database connection + server start
 mongoose
